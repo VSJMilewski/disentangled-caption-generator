470866d5184fe2c8cebd52df29cd6420cc422038
Victor Milewski <milewgpu@test3.irc.sara.nl.irc.sara.nl>
2018-09-05 14:25:48 +0200

    fixed script and typo in main

--- jobscript.sh
+++ jobscript.sh
@@ -2,13 +2,25 @@
 #PBS -lnodes=1
 #PBS -lwalltime=00:05:00
 
-tar zcvf $HOME/multimodal-descriptions/data.tar.gz $HOME/multimodal-descriptions/data
-cd "$TMPDIR"
+echo "creating a tar for the data"
+tar zcf $HOME/multimodal-descriptions/data.tar.gz -C $HOME/multimodal-descriptions/ data
+echo "moving the pickles to scratch"
 cp $HOME/multimodal-descriptions/*.pkl "$TMPDIR"
-tar zxf $HOME/multimodal-descriptions/data.tar.gz
+echo "untar the data into scratch"
+tar zxf $HOME/multimodal-descriptions/data.tar.gz -C "$TMPDIR"
+echo "make output dir"
 mkdir "$TMPDIR"/output
 
+echo "move into scratch"
+cd "$TMPDIR"
+ls -l
+
+echo "run program..."
+echo ""
 
 python3 $HOME/multimodal-descriptions/main.py
 
+echo ""
+echo ""
+echo "tarring output to home"
 tar zcf $HOME/multimodal-descriptions/output_$HOSTNAME.tar.gz output
