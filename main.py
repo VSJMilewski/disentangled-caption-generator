9dea367cdc9b5820bf94dd801b1029d34a3cb707
victor <victormilewski@gmail.com>
2018-10-31 16:33:37 +0100

    output file

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import matplotlib
-
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
-
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -138,55 +136,46 @@ def validation_step(model, data_set, processor, max_seq_length, pred_file, ref_f
     return score
 
 
-def print_info():
-    print('=' * 80 + '\n{:8s}\t {:6s}\t {:6s}\t {:6s}\t {:6s}\t {:6s}\t {:6s}\t {:6s}\t{:7s}\n{}'.format(
-        'EPOCH:', 'BLEU1', 'BLEU2', 'BLEU3', 'BLEU4', 'METEOR', 'ROGUEl', 'CIDer', 'Time', '=' * 80))
+def print_info(file):
+    with open(file, 'a') as f:
+        print('=' * 80 + '\n{:8s}\t {:6s}\t {:6s}\t {:6s}\t {:6s}\t {:6s}\t {:6s}\t {:6s}\t{:7s}\n{}'.format(
+            'EPOCH:', 'BLEU1', 'BLEU2', 'BLEU3', 'BLEU4', 'METEOR', 'ROGUEl', 'CIDer', 'Time', '=' * 80), file=f)
 
 
-def print_score(score, time_, epoch):
-    print('{:5d}   \t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:7.3f}s'.format(
-        epoch, score['Bleu_1'] * 100, score['Bleu_2'] * 100, score['Bleu_3'] * 100, score['Bleu_4'] * 100,
-               score['METEOR'] * 100, score['ROUGE_L'] * 100, score['CIDEr'] * 100, time_))
+def print_score(score, time_, epoch, file):
+    with open(file, 'a') as f:
+        print('{:5d}   \t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}\t{:7.3f}s'.format(
+            epoch, score['Bleu_1'] * 100, score['Bleu_2'] * 100, score['Bleu_3'] * 100, score['Bleu_4'] * 100,
+                   score['METEOR'] * 100, score['ROUGE_L'] * 100, score['CIDEr'] * 100, time_), file=f)
 
 
 def train():
-    PAD = config.pad
-    START = config.sos
-    END = config.eos
-    UNK = config.unk
-    vocab_size = config.vocab_size
-    max_seq_length = config.max_seq_length
-    learning_rate = config.learning_rate
-    max_epochs = config.max_epochs
-    batch_size = config.batch_size  # 5 images per sample, 13x5=65, 25x5=125
-    eval_batch_size = config.eval_batch_size
-    beam_size = config.beam_size
-    embedding_size = config.num_hidden
-    patience = config.patience
-    base_output_path = config.output_path
-    base_data_path = config.data_path
-    base_pickle_path = config.pickle_path
-
     # temporary files
-    prediction_file = os.path.join(base_output_path, '{}_{}_beam{}_{}.pred'.format(config.model,
-                                                                                   config.dataset,
-                                                                                   config.beam_size,
-                                                                                   config.unique))
+    prediction_file = os.path.join(config.output_path, '{}_{}_beam{}_{}.pred'.format(config.model,
+                                                                                     config.dataset,
+                                                                                     config.beam_size,
+                                                                                     config.unique))
+    progress_file = os.path.join(config.output_path, '{}_{}_beam{}_{}.out'.format(config.model,
+                                                                                  config.dataset,
+                                                                                  config.beam_size,
+                                                                                  config.unique))
 
     # output files
-    last_epoch_file = os.path.join(base_output_path, 'last_{}_beam{}_{}_{}.pkl'.format(config.dataset,
-                                                                                       config.beam_size,
-                                                                                       config.model,
-                                                                                       config.unique))
-    best_epoch_file = os.path.join(base_output_path, 'best_{}_beam{}_{}_{}.pkl'.format(config.dataset,
-                                                                                       config.beam_size,
-                                                                                       config.model,
-                                                                                       config.unique))
+    last_epoch_file = os.path.join(config.output_path, 'last_{}_beam{}_{}_{}.pkl'.format(config.dataset,
+                                                                                         config.beam_size,
+                                                                                         config.model,
+                                                                                         config.unique))
+    best_epoch_file = os.path.join(config.output_path, 'best_{}_beam{}_{}_{}.pkl'.format(config.dataset,
+                                                                                         config.beam_size,
+                                                                                         config.model,
+                                                                                         config.unique))
 
     # pickle files
-    train_vocab_file = os.path.join(base_pickle_path, 'train_{}_vocab_{}.pkl'.format(config.dataset, vocab_size))
-    train_data_file = os.path.join(base_pickle_path, 'data_{}_train.pkl'.format(config.dataset))
-    dev_data_file = os.path.join(base_pickle_path, 'data_{}_dev.pkl'.format(config.dataset))
+    train_vocab_file = os.path.join(config.pickle_path, 'train_{}_vocab_{}_th_{}.pkl'.format(config.dataset,
+                                                                                             config.vocab_size,
+                                                                                             config.vocab_threshold))
+    train_data_file = os.path.join(config.pickle_path, 'data_{}_train_th_{}.pkl'.format(config.dataset))
+    dev_data_file = os.path.join(config.pickle_path, 'data_{}_dev_th_{}.pkl'.format(config.dataset))
 
     # data files
     train_images_file = None
@@ -195,17 +184,17 @@ def train():
     reference_file = None
     captions_file = None
     if config.dataset == 'flickr8k':
-        train_images_file = os.path.join(base_data_path, 'flickr8k/Flickr_8k.trainImages.txt')
-        dev_images_file = os.path.join(base_data_path, 'flickr8k/Flickr_8k.devImages.txt')
-        base_path_images = os.path.join(base_data_path, 'flickr8k/Flicker8k_Dataset/')
-        reference_file = os.path.join(base_data_path, 'flickr8k/Flickr8k_references.dev.json')
-        captions_file = os.path.join(base_data_path, 'flickr8k/Flickr8k.token.txt')
+        train_images_file = os.path.join(config.data_path, 'flickr8k/Flickr_8k.trainImages.txt')
+        dev_images_file = os.path.join(config.data_path, 'flickr8k/Flickr_8k.devImages.txt')
+        base_path_images = os.path.join(config.data_path, 'flickr8k/Flicker8k_Dataset/')
+        reference_file = os.path.join(config.data_path, 'flickr8k/Flickr8k_references.dev.json')
+        captions_file = os.path.join(config.data_path, 'flickr8k/Flickr8k.token.txt')
     elif config.dataset == 'flickr30k':
-        train_images_file = os.path.join(base_data_path, 'flickr30k/flickr30k.trainImages.txt')
-        dev_images_file = os.path.join(base_data_path, 'flickr30k/flickr30k.devImages.txt')
-        base_path_images = os.path.join(base_data_path, 'flickr30k/flickr30k_images/')
-        reference_file = os.path.join(base_data_path, 'flickr30k/flickr30k_references.dev.json')
-        captions_file = os.path.join(base_data_path, 'flickr30k/results_20130124.token')
+        train_images_file = os.path.join(config.data_path, 'flickr30k/flickr30k.trainImages.txt')
+        dev_images_file = os.path.join(config.data_path, 'flickr30k/flickr30k.devImages.txt')
+        base_path_images = os.path.join(config.data_path, 'flickr30k/flickr30k_images/')
+        reference_file = os.path.join(config.data_path, 'flickr30k/flickr30k_references.dev.json')
+        captions_file = os.path.join(config.data_path, 'flickr30k/results_20130124.token')
     else:
         exit('Unknown dataset')
 
@@ -218,25 +207,28 @@ def train():
         annotations = f.read().splitlines()
 
     # data processor for vocab and their index mappings
-    processor = DataProcessor(annotations, train_images, filename=train_vocab_file, vocab_size=vocab_size,
-                              pad=PAD, start=START, end=END, unk=UNK, vocab_threshold=config.vocab_threshold)
+    processor = DataProcessor(annotations, train_images, filename=train_vocab_file, vocab_size=config.vocab_threshold,
+                              pad=config.pad, start=config.sos, end=config.eos, unk=config.unk,
+                              vocab_threshold=config.vocab_threshold)
 
     # data files containing the data and handling batching
-    train_data = data(base_path_images, train_images, annotations, max_seq_length, processor, train_data_file, START,
-                      END)
-    dev_data = data(base_path_images, dev_images, annotations, max_seq_length, processor, dev_data_file, START, END)
+    train_data = data(base_path_images, train_images, annotations, config.max_seq_length, processor, train_data_file,
+                      config.sos,
+                      config.eos)
+    dev_data = data(base_path_images, dev_images, annotations, config.max_seq_length, processor, dev_data_file,
+                    config.sos, config.eos)
 
     # create the models
     model = None
     if config.model == 'BASELINE':
-        model = CaptionModel(embedding_size, processor.vocab_size, device).to(device)
+        model = CaptionModel(config.num_hidden, processor.vocab_size, device).to(device)
     # elif config.model == 'BINARY':
-    #     model = BinaryCaptionModel(embedding_size, processor.vocab_size, device).to(device)
+    #     model = BinaryCaptionModel(config.num_hidden, processor.vocab_size, device).to(device)
     else:
         exit('not an existing model!')
     # params = list(model.encoder.inception.fc.parameters()) + list(model.decoder.parameters())
     params = filter(lambda p: p.requires_grad, model.parameters())
-    opt = Adam(params, lr=learning_rate)
+    opt = Adam(params, lr=config.learning_rate)
 
     # Start training
     losses = []
@@ -248,11 +240,11 @@ def train():
     number_up = 0
     print('training started!')
     start0 = time.time()
-    for epoch in range(max_epochs):
+    for epoch in range(config.max_epochs):
         start = time.time()
 
         # loop over all the training batches in the epoch
-        for i_batch, batch in enumerate(batch_generator(train_data, batch_size, transform_train, device)):
+        for i_batch, batch in enumerate(batch_generator(train_data, config.batch_size, transform_train, device)):
             opt.zero_grad()
             image, caption, caption_lengths, _ = batch
             image = image.to(device)
@@ -261,16 +253,16 @@ def train():
             loss = model(image, caption, caption_lengths)
             loss.backward()
             losses.append(float(loss))
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
+            torch.nn.utils.clip_grad_value_(model.parameters(), max_norm=config.max_grad)
             opt.step()
         # store epoch results
         avg_losses[len(losses)] = np.mean(losses[loss_current_ind:])
         loss_current_ind = len(losses)
 
         # validation
-        score = validation_step(model, dev_data, processor, max_seq_length, prediction_file, reference_file,
-                                beam_size=beam_size, batch_size=eval_batch_size,
-                                pad=PAD, start=START, end=END)
+        score = validation_step(model, dev_data, processor, config.max_seq_length, prediction_file, reference_file,
+                                beam_size=config.beam_size, batch_size=config.eval_batch_size,
+                                pad=config.pad, start=config.sos, end=config.eos)
         scores.append(score)
         torch.save(model.cpu().state_dict(), last_epoch_file)
         model = model.to(device)
@@ -278,7 +270,7 @@ def train():
         # test termination
         if score[config.eval_metric] <= best_bleu:
             number_up += 1
-            if number_up > patience:
+            if number_up > config.patience:
                 break
         else:
             number_up = 0
@@ -290,28 +282,30 @@ def train():
         # print some info
         end = time.time()
         if epoch % 50 == 0:
-            print_info()
-        print_score(scores[-1], end - start, epoch)
+            print_info(progress_file)
+        print_score(scores[-1], end - start, epoch, progress_file)
 
-    print_info()
-    print('\n\n\t\t --- Best Epoch: ---')
-    print_info()
-    print_score(scores[best_epoch], time.time() - start0, best_epoch)
-    print('=' * 80)
+    print_info(progress_file)
+    with open(progress_file, 'a') as f:
+        print('\n\n\t\t --- Best Epoch: ---', file=f)
+    print_info(progress_file)
+    print_score(scores[best_epoch], time.time() - start0, best_epoch, progress_file)
+    with open(progress_file, 'a') as f:
+        print('=' * 80, file=f)
 
-    pickle.dump(scores, open(os.path.join(base_output_path,
+    pickle.dump(scores, open(os.path.join(config.output_path,
                                           'scores_{}_baseline_model_epoch_{}_beam{}_{}.pkl'.format(config.dataset,
                                                                                                    epoch,
                                                                                                    config.beam_size,
                                                                                                    config.unique)),
                              'wb'))
-    pickle.dump(losses, open(os.path.join(base_output_path,
+    pickle.dump(losses, open(os.path.join(config.output_path,
                                           'losses_{}_baseline_model_epoch_{}_beam{}_{}.pkl'.format(config.dataset,
                                                                                                    epoch,
                                                                                                    config.beam_size,
                                                                                                    config.unique)),
                              'wb'))
-    pickle.dump(avg_losses, open(os.path.join(base_output_path,
+    pickle.dump(avg_losses, open(os.path.join(config.output_path,
                                               'avg_losses_{}_baseline_model_epoch_{}_beam{}_{}.pkl'.format(
                                                   config.dataset,
                                                   epoch,
@@ -332,7 +326,7 @@ if __name__ == "__main__":
     parser.add_argument('--vocab_size', type=int, default=30000, help='Max size of the vocabulary')
     parser.add_argument('--patience', type=int, default=10, help='Patience before terminating')
     parser.add_argument('--max_seq_length', type=int, default=100, help='Length of an input sequence')
-    parser.add_argument('--learning_rate', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('--learning_rate', type=float, default=4e-4, help='Learning rate')
     parser.add_argument('--num_hidden', type=int, default=512, help='Number of hidden units in the LSTM')
     parser.add_argument('--num_layers', type=int, default=1, help='Number of LSTM layers in the model')
     parser.add_argument('--batch_size', type=int, default=25, help='Number of samples in batch, 5 sentences per sample')
@@ -347,7 +341,7 @@ if __name__ == "__main__":
     parser.add_argument('--model', type=str, default='BASELINE', help='which model to use: BASELINE, BINARY')
     parser.add_argument('--dataset', type=str, default='flickr8k', help='flickr8k, flickr30k, coco(not ready yet)')
     parser.add_argument('--device', type=str, default='cuda', help='On which device to run, cpu, cuda or None')
-    parser.add_argument('--max_norm', type=float, default=0.25, help='max norm for gradients')
+    parser.add_argument('--max_grad', type=float, default=5, help='max value for gradients')
     parser.add_argument('--vocab_threshold', type=int, default=5, help='minimum number of occurances to be in vocab')
     parser.add_argument('--eval_metric', type=str, default='Bleu_4', help='on which metric to do early stopping')
     parser.add_argument('--unique', type=str, default='', help='string to make files unique')
