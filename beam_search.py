fe310849657f3ed750863ca2d7dee2b1e63291c1
victormilewski1994 <victormilewski@gmail.com>
2018-11-14 19:17:48 +0100

    beamsearch now terminates when all paths end

--- beam_search.py
+++ beam_search.py
@@ -29,6 +29,9 @@ class Beam(object):
         # The score for each translation on the beam.
         self.scores = torch.FloatTensor(size).zero_().to(device)
 
+        # mask for ending advancing finished sequences. -inf if done, 1 otherwise
+        self.mask = torch.FloatTensor(size).fill_(1).to(device)
+
         # The backpointers at each time-step.
         self.prevKs = []
 
@@ -47,24 +50,30 @@ class Beam(object):
         """Get the backpointer to the beam at this step."""
         return self.prevKs[-1]
 
-    def advance(self, workd_lk, attn_out=None):
+    def advance(self, word_lk, attn_out=None):
         """
         Given prob over words for every last beam `wordLk` and attention `attnOut`: Compute and update the beam search.
-        :param workd_lk: probs of advancing from the last step (K x words)
+        :param word_lk: probs of advancing from the last step (K x words)
         :param attn_out: attention at the last step
         :return: True if beam search is complete.
         """
-        num_words = workd_lk.size(1)
+        num_words = word_lk.size(1)
+
+        # apply mask over the scores so finished sequences wont get expanded
+        word_lk = self.mask.unsqueeze(1).expand_as(word_lk) * word_lk
+        # expanding with padding is free if the end of the sentence is reached. This ensures
+        # that short sentence can also be predicted
+        word_lk[(self.mask < 0).nonzero(), self.processor.PAD] = 0
 
         # Sum the previous scores.
         if len(self.prevKs) > 0:
-            beam_lk = workd_lk + self.scores.unsqueeze(1).expand_as(workd_lk)
+            beam_lk = word_lk + self.scores.unsqueeze(1).expand_as(word_lk)
         else:
-            beam_lk = workd_lk[0]
+            beam_lk = word_lk[0]
 
         flat_beam_lk = beam_lk.view(-1)
 
-        best_scores, best_scores_id = flat_beam_lk.topk(self.size, 0, True, True)
+        best_scores, best_scores_id = flat_beam_lk.topk(self.size, dim=0, largest=True, sorted=True)
         self.scores = best_scores
 
         # best_scores_id is flattened beam x word array, so calculate which
@@ -73,9 +82,20 @@ class Beam(object):
         self.prevKs.append(prev_k)
         self.nextYs.append(best_scores_id - prev_k * num_words)
 
-        # End condition is when top-of-beam is EOS.
-        if self.nextYs[-1][0] == self.processor.w2i[self.processor.END]:
+        # Update sequence scores and erase scores for end-of-sentence symbol so that they aren't expanded
+        # stored_scores.append(best_scores.clone())
+        eos_indices = self.nextYs[-1].data.eq(self.processor.w2i[self.processor.END])
+        if eos_indices.nonzero().dim() > 0:
+            self.mask.fill_(1) # reset mask, finished seq might be dropped
+            self.mask.data.masked_fill_(eos_indices, -float('inf'))
+
+        # end condition when all paths reached EOS
+        if (self.mask == 1).nonzero():
             self.done = True
+
+        # # End condition is when top-of-beam is EOS.
+        # if self.nextYs[-1][0] == self.processor.w2i[self.processor.END]:
+        #     self.done = True
 
         return self.done
 
