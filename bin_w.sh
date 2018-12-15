e83c398e773ad3ffdb90f189bc28be5f28b75038
Victor Milewski <milewgpu@test3.irc.sara.nl.irc.sara.nl>
2018-12-15 14:51:24 +0100

    changed run files

---
+++ bin_w.sh
@@ -0,0 +1,32 @@
+#PBS -qgpu
+#PBS -lnodes=1
+#PBS -lwalltime=5:00:00
+
+module load eb/3.7.0
+module load python/3.5.0
+module load cuDNN/7.3.1-CUDA-10.0.130
+
+
+echo "creating a tar for the data and pickles"
+tar zcf $HOME/multimodal-descriptions/data.tar.gz -C $HOME/multimodal-descriptions/ data
+#echo "moving the pickles to scratch"
+cp -r $HOME/multimodal-descriptions/pickles/ "$TMPDIR"
+echo "untar the data into scratch"
+tar zxf $HOME/multimodal-descriptions/data.tar.gz -C "$TMPDIR"
+echo "make output dir"
+mkdir "$TMPDIR"/output
+
+echo "move into scratch"
+cd "$TMPDIR"
+ls -l
+
+echo "run program..."
+echo ""
+
+python3 $HOME/multimodal-descriptions/main.py --beam_size 1 --model BINARY --dataset flickr8k --device cuda --num_workers 16 --batch_size 256 --min_epochs 0 --max_epochs 60 --patience 10 --vocab_threshold 5 --max_time 169200 --max_grad 10 --optimizer Adam --binary_train_method WEIGHTED --learning_rate 0.001
+
+echo ""
+echo ""
+echo "tarring output to home"
+tar zcf $HOME/multimodal-descriptions/output_$PBS_JOBID.tar.gz output pickles
+echo "Job $PBS_JOBID, bin_wl ended at `date`" | mail $USER -s "Job $PBS_JOBID"
